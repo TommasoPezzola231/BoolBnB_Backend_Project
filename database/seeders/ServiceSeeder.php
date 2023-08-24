f3e8171464ff4eed643bc18f23591f76b63a1b38
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-24 17:20:25 +0200

    aggiunto array servizi

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,94 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $services = [
+            [
+                'name_service' => 'wi-fi',
+                'icon_service' => 'fa-solid fa-wifi',
+            ],
+            [
+                'name_service' => 'Piscina',
+                'icon_service' => 'fa-solid fa-person-swimming',
+            ],
+            [
+                'name_service' => 'Palestra',
+                'icon_service' => 'fa-solid fa-dumbbell',
+            ],
+            [
+                'name_service' => 'Sauna',
+                'icon_service' => 'fa-solid fa-hot-tub-person',
+            ],
+            [
+                'name_service' => 'Vista mare',
+                'icon_service' => 'fa-solid fa-water',
+            ],
+            [
+                'name_service' => 'Posto auto',
+                'icon_service' => 'fa-solid fa-square-parking',
+            ],
+            [
+                'name_service' => 'Aria condizionata',
+                'icon_service' => 'fa-solid fa-fan',
+            ],
+            [
+                'name_service' => 'Riscaldamento',
+                'icon_service' => 'fa-solid fa-temperature-arrow-up',
+            ],
+            [
+                'name_service' => 'Colazione',
+                'icon_service' => 'fa-solid fa-mug-saucer',
+            ],
+            [
+                'name_service' => 'TV',
+                'icon_service' => 'fa-solid fa-tv',
+
+            ],
+            [
+                'name_service' => 'Cucina',
+                'icon_service' => 'fa-solid fa-kitchen-set',
+            ],
+            [
+                'name_service' => 'Lavatrice',
+                'icon_service' => 'fa-solid fa-soap',
+
+            ],
+            [
+                'name_service' => 'Ferro da stiro',
+                'icon_service' => 'fa-solid fa-shirt',
+
+            ],
+            [
+                'name_service' => 'Asciugacapelli',
+                'icon_service' => 'fa-solid fa-wind',
+
+            ],
+            [
+                'name_service' => 'fumo permesso',
+                'icon_service' => 'fa-solid fa-smoking',
+
+            ],
+            [
+                'name_service' => 'Kit di pronto soccorso',
+                'icon_service' => 'fa-solid fa-kit-medical',
+
+            ],
+            [
+                'name_service' => 'Animali domestici ammessi',
+                'icon_service' => 'fa-solid fa-dog',
+
+            ],
+            [
+                'name_service' => 'Camino',
+                'icon_service' => 'fa-solid fa-fire',
+
+            ],
+        ];
+
+        foreach ($services as $service) {
+            $newService = new Service();
+            $newService->name_service = $service['name_service'];
+            $newService->icon_service = $service['icon_service'];
+            $newService->save();
+        }
     }
 }
