433ef5688d8a41aad45dfa0cc9569ba80c22d7a2
TommasoPezzola <pezzola123@gmail.com>
2023-08-24 18:23:26 +0200

    Aggiunto $fillable e relazione con apartments

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "apartment_id",
+        "sender_email",
+        "message_text",
+        "name_sender",
+        "sent_at"
+    ];
+
+    public function apartments() {
+        return $this->belongsTo(Apartment::class);
+    }
 }
