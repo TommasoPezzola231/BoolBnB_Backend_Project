eeb2514e1abe8855a2070a9685c6937f0d19fbc3
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-24 15:40:27 +0200

    aggiunto migration sponsorship

---
+++ database/migrations/2023_08_24_133447_create_sponsorships_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_sponsorship');
+            $table->smallInteger('duration');
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
