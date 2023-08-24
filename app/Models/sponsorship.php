c1c2799c343338c748ce5d13a2e09775e6963595
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-24 18:12:33 +0200

    aggiunto fillable dati

--- app/Models/sponsorship.php
+++ app/Models/sponsorship.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sponsorship extends Model
+class Sponsorship extends Model
 {
+
+    protected $fillable = [
+        'name_sponsorship',
+        'price',
+        'duration',
+    ];
+
     use HasFactory;
 }
