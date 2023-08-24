77bc56d0ce3ec2bc6e4599904f2f925f90a94007
TommasoPezzola <pezzola123@gmail.com>
2023-08-24 18:57:39 +0200

    Aggiunta relazione images

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -35,4 +35,8 @@ class Apartment extends Model
     {
         return $this->hasMany(Message::class);
     }
+
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
 }
