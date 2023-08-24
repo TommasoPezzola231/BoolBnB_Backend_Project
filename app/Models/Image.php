548c281b320685bf7710bf60df5f1a695f7002f5
TommasoPezzola <pezzola123@gmail.com>
2023-08-24 18:58:17 +0200

    Aggiunti $fillable e relazione apartment

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "Image_url",
+        "apartment_id"
+    ];
+
+    public function apartment() {
+        return $this->belongsTo(Apartment::class);
+    }
 }
