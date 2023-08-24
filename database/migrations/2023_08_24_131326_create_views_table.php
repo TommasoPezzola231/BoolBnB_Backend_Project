17253128f0ca56184daa2c6e49431b198bbe8fa0
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-24 15:33:46 +0200

    aggiunot migration view

---
+++ database/migrations/2023_08_24_131326_create_views_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained();
+            $table->string('ip', 20);
+            $table->dateTime('viewed_at',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+};
