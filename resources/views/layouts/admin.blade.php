63eb7e2ecdee63a8d047c011db59585b0d4a2672
TommasoPezzola <pezzola123@gmail.com>
2023-08-24 12:28:12 +0200

    Aggiunto nav-item

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,6 +43,12 @@
                             </li>
 
                             <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.appartments.create' ? 'bg-secondary' : '' }}" href="{{route('admin.appartments.create')}}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Aggiungi Casa
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     <i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
                                 </a>
