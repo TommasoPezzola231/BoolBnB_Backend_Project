f0d90e6fe9e59d5f4c86c5f6ba73c1d7eb71200f
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-08-24 18:34:25 +0200

    add $fillable and relationship

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "principal_image",
+        "imageID",
+        "description",
+        "price",
+        "country",
+        "num_rooms",
+        "num_bathrooms",
+        "square_meters",
+        "address",
+        "serviceID",
+        "visible",
+        "latitude",
+        "longitude"
+    ];
+
+    public function services()
+    {
+        return $this->belongToMany(Service::class);
+    }
 }
