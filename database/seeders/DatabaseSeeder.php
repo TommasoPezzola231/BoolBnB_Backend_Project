2b751cf76d8033e1f08ce499110db02a9dde99e3
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-08-24 18:35:09 +0200

    Add call() method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            ApartmentSeeder::class,
+            MessageSeeder::class,
+            ServiceSeeder::class,
+            SponsorshipSeeder::class
+        ]);
     }
 }
