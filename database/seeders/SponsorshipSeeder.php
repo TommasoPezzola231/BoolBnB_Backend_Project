8139c4758924b7639ae45de732784d21ada8706f
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-24 18:12:45 +0200

    aggiunto aray sponsorships

--- database/seeders/SponsorshipSeeder.php
+++ database/seeders/SponsorshipSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sponsorship;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,30 @@ class SponsorshipSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $sponsorships = [
+            [
+                'name_sponsorship' => 'Bronzo',
+                'price' => 2.99,
+                'duration' => 24,
+            ],
+            [
+                'name_sponsorship' => 'Argento',
+                'price' => 5.99,
+                'duration' => 72,
+            ],
+            [
+                'name_sponsorship' => 'Oro',
+                'price' => 9.99,
+                'duration' => 144,
+            ],
+        ];
+
+        foreach ($sponsorships as $sponsorship) {
+            $newSponsorship = new Sponsorship();
+            $newSponsorship->name_sponsorship = $sponsorship['name_sponsorship'];
+            $newSponsorship->price = $sponsorship['price'];
+            $newSponsorship->duration = $sponsorship['duration'];
+            $newSponsorship->save();
+        }
     }
 }
