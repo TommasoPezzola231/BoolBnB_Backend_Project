a403d51cab8066e938ffc09784a6ad6f9a4c3bcb
TommasoPezzola <pezzola123@gmail.com>
2023-08-25 15:54:59 +0200

    Aggiunta rotta index

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,6 +49,12 @@
                             </li>
 
                             <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.apartments.index' ? 'bg-secondary' : '' }}" href="{{route('admin.apartments.index')}}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Elenco Case
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     <i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
                                 </a>
