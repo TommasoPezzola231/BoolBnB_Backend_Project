fc02f565985da837fbc7d3ce98d85166ecf23ef2
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-25 09:47:26 +0200

    prova seeder view

--- database/seeders/ViewSeeder.php
+++ database/seeders/ViewSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\View;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
 
 class ViewSeeder extends Seeder
 {
@@ -12,8 +17,19 @@ class ViewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        // for ($i = 0; $i < 10000; $i++) {
+
+        //     $apartment_id = Apartment::inRandomOrder()->first()->id;
+
+        //     $view = new View();
+
+        //     $view->apartment_id = $apartment_id;
+        //     $view->ip = $faker->ipv4;
+        //     $view->viewed_at = $faker->dateTime('now');
+
+        //     $view->save();
+        // }
     }
 }
