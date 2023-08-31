6983ff61c606124ce8046209612f439b708b1313
TommasoPezzola <pezzola123@gmail.com>
2023-08-31 11:03:56 +0200

    aggiunta Api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
@@ -25,6 +25,43 @@ class ApartmentController extends Controller
     }
 
     public function search(Request $request)
+    {
+        $city = $request->input('city');
+
+        $url = 'https://api.tomtom.com/search/2/geocode/' . urlencode($city) . '.json';
+        $apiKey = 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP';
+
+        $response = Http::get($url, [
+            'key' => $apiKey
+        ]);
+
+        // $response = Http::get($url, [
+        //     'key' => 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP' //env('KEY_TOMTOM')
+        // ]);
+
+        $data_api = $response->json();
+
+        $latitudine = $data_api['results'][0]['position']['lat'];
+        $longitudine = $data_api['results'][0]['position']['lon'];
+
+        $cityCoordinates = ['latitude' => $latitudine, 'longitude' => $longitudine];
+
+        // Calcola le coordinate dei limiti del rettangolo (approssimato)
+        $minLatitude = $cityCoordinates['latitude'] - 0.18; // Approssimazione per 20 km
+        $maxLatitude = $cityCoordinates['latitude'] + 0.18;
+        $minLongitude = $cityCoordinates['longitude'] - 0.18;
+        $maxLongitude = $cityCoordinates['longitude'] + 0.18;
+
+        // Filtra gli appartamenti
+        $apartments = Apartment::whereBetween('latitude', [$minLatitude, $maxLatitude])
+            ->whereBetween('longitude', [$minLongitude, $maxLongitude])
+            ->get();
+
+        return response()->json(['apartments' => $apartments]);
+        
+    }
+
+    public function searchPlus(Request $request)
     {
         $radius = $request->input('radius', 20); // Raggio di default di 20 km
 
@@ -51,17 +88,24 @@ class ApartmentController extends Controller
             'latitude' => $request->input('latitude'),
             'longitude' => $request->input('longitude')
         ];
-
-        /*controllo per la validità dei dati numerici di $address
-        query che calcola la distanza tra una posizione geografica di riferimento e le posizioni nel database*/
+        //controllo per la validità dei dati numerici di $address
+        //query che calcola la distanza tra una posizione geografica di riferimento e le posizioni nel database
         if (is_numeric($address['latitude']) && is_numeric($address['longitude'])) {
-            $query->select(DB::raw("*,
-                (6371 * acos(cos(radians({$address['latitude']})) * cos(radians(latitude)) *
+            $query->select(DB::raw(",
+                (6371 acos(cos(radians({$address['latitude']})) * cos(radians(latitude)) *
                 cos(radians(longitude) - radians({$address['longitude']})) +
                 sin(radians({$address['latitude']})) * sin(radians(latitude))))
                 AS distance"))
                 ->having('distance', '<', $radius);
         }
+
+        $serviceName = 'name_service';
+        $apartmentIdService = DB::table('apartment_service')
+            ->join('services', 'apartment_service.service_id', '=', 'services.id')
+            ->where('services.name_service', $serviceName)
+            ->pluck('apartment_service.apartment_id'); //ottengo elenco ID appartamento dalla tabella pivot
+
+        $query->whereIn('id', $apartmentIdService); //filtro gli appartamenti nella query principale (aparment::query)
 
         $apartments = $query->get();
 
