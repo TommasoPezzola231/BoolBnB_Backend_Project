d1052781899ef67a59ff0e1777e40c896501450b
TommasoPezzola <pezzola123@gmail.com>
2023-08-31 19:25:15 +0200

    fix

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -71,37 +71,64 @@ class ApartmentController extends Controller
     {
         $city = $request->input('city');
         
-        if ($city) {
-            $url = 'https://api.tomtom.com/search/2/geocode/' . urlencode($city) . '.json';
-            $apiKey = 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP';
+        
+        $url = 'https://api.tomtom.com/search/2/geocode/' . urlencode($city) . '.json';
+        $apiKey = 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP';
 
-            $response = Http::get($url, [
-                'key' => $apiKey
-            ]);
+        $response = Http::get($url, [
+            'key' => $apiKey
+        ]);
 
-            // $response = Http::get($url, [
-            //     'key' => 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP' //env('KEY_TOMTOM')
-            // ]);
+        // $response = Http::get($url, [
+        //     'key' => 'U6BQ1DicdzYIkj5nrK4823OxJuCY6gyP' //env('KEY_TOMTOM')
+        // ]);
 
-            $data_api = $response->json();
+        $data_api = $response->json();
 
-            $latitudine = $data_api['results'][0]['position']['lat'];
-            $longitudine = $data_api['results'][0]['position']['lon'];
+        $latitudine = $data_api['results'][0]['position']['lat'];
+        $longitudine = $data_api['results'][0]['position']['lon'];
 
-            $cityCoordinates = ['latitude' => $latitudine, 'longitude' => $longitudine];
+        $cityCoordinates = ['latitude' => $latitudine, 'longitude' => $longitudine];
 
-            $radius = $request->input('radius', 20);
-            $radiusInDegrees = $radius / 111.32;
+        $radius = $request->input('radius', 20);
+        $radiusInDegrees = $radius / 111.32;
 
-            // Calcola le coordinate dei limiti del rettangolo (approssimato)
-            $minLatitude = $cityCoordinates['latitude'] - $radiusInDegrees; // Approssimazione per 20 km
-            $maxLatitude = $cityCoordinates['latitude'] + $radiusInDegrees;
-            $minLongitude = $cityCoordinates['longitude'] - $radiusInDegrees;
-            $maxLongitude = $cityCoordinates['longitude'] + $radiusInDegrees;
+        // Calcola le coordinate dei limiti del rettangolo (approssimato)
+        $minLatitude = $cityCoordinates['latitude'] - $radiusInDegrees; // Approssimazione per 20 km
+        $maxLatitude = $cityCoordinates['latitude'] + $radiusInDegrees;
+        $minLongitude = $cityCoordinates['longitude'] - $radiusInDegrees;
+        $maxLongitude = $cityCoordinates['longitude'] + $radiusInDegrees;
 
-            $apartments = Apartment::whereBetween('latitude', [$minLatitude, $maxLatitude])
+        $numRooms = $request->input('num_rooms');
+        $numBathrooms = $request->input('num_bathrooms');
+        $squareMeters = $request->input('square_meters');
+        $price = $request->input('price');
+        $selectedServices = $request->input('serviceID', []);
+
+        $apartments = Apartment::whereBetween('latitude', [$minLatitude, $maxLatitude])
             ->whereBetween('longitude', [$minLongitude, $maxLongitude]);
-        }
+            if ($numRooms) {
+                $apartments->where('num_rooms', '>=', $request->input('num_rooms'));
+            }
+
+            if ($numBathrooms) {
+                $apartments->where('num_bathrooms', '>=', $request->input('num_bathrooms'));
+            }
+
+            if ($squareMeters) {
+                $apartments->where('square_meters', '>=', $request->input('square_meters'));
+            }
+
+            if ($price) {
+                $apartments->where('price', '<=', $request->input('price'));
+            }
+
+            if ($selectedServices) {
+                $apartments->whereHas('services', function ($query) use ($selectedServices) {
+                    $query->whereIn('service_id', $selectedServices);
+                });
+            }
+        
 
         /*$city = $request->input('city');
         if ($city) {
@@ -109,36 +136,24 @@ class ApartmentController extends Controller
         }*/
 
 
-        $numRooms = $request->input('num_rooms');
-        if ($numRooms) {
-            $apartments = Apartment::where('num_rooms', '>=', $request->input('num_rooms'));
-        }
+        
+        
 
 
-        $numBathrooms = $request->input('num_bathrooms');
-        if ($numBathrooms) {
-            $apartments = Apartment::where('num_bathrooms', '>=', $request->input('num_bathrooms'));
-        }
+        
+        
 
 
-        $squareMeters = $request->input('square_meters');
-        if ($squareMeters) {
-            $apartments = Apartment::where('square_meters', '>=', $request->input('square_meters'));
-        }
+        
+        
 
 
-        $price = $request->input('price');
-        if ($price) {
-            $apartments = Apartment::where('price', '<=', $request->input('price'));
-        }
+        
+        
 
 
-        $selectedServices = $request->input('serviceID', []);
-        if ($selectedServices) {
-            $apartments = Apartment::whereHas('services', function ($query) use ($selectedServices) {
-                $query->whereIn('service_id', $selectedServices);
-            });
-        }
+        
+        
 
 
 
