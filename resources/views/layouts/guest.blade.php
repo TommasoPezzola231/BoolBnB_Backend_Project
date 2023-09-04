c9909a447f2602c4c86d6c0561622ea78fc2eda2
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-09-04 16:14:27 +0200

    redirect login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -27,4 +27,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
