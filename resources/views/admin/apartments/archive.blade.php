cc7bb975d62cf4fd11c315b674565165dfad3c18
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-09-06 11:45:37 +0200

    fix

--- resources/views/admin/apartments/archive.blade.php
+++ resources/views/admin/apartments/archive.blade.php
@@ -3,38 +3,43 @@
 @section('content')
 
     <section>
-
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="my-3">Appartamenti eliminati</h2>
-                    <hr>
-                    <table class="table table-bordered mt-3">
-                        <thead>
-                          <tr class="table-secondary">
-                            <th scope="col">Titolo</th>
-                            <th scope="col">Indirizzo</th>
-                            <th scope="col">Città</th>
-                            <th scope="col">Azioni</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($deletedApartments as $deletedApartment)
-                                <tr class="table-danger">
-                                    <td>{{$deletedApartment->title}}</td>
-                                    <td>{{$deletedApartment->address}}</td>
-                                    <td>{{$deletedApartment->city}}</td>
-                                    <td>
-                                        <a href="{{route('admin.apartments.restore', $deletedApartment->id)}}" class="btn btn-success">Ripristina</a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                      </table>
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <h2 class="my-3">Appartamenti eliminati</h2>
+                        <hr>
+                        @if ($deletedApartments->isEmpty())
+                            <div class="col-12 text-center">
+                                <h3 class="text-white">Non hai nessun appartamento archiviato</h3>
+                            </div>
+                        @else
+                            <table class="table table-bordered mt-3">
+                                <thead>
+                                    <tr class="table-secondary">
+                                        <th scope="col">Titolo</th>
+                                        <th scope="col">Indirizzo</th>
+                                        <th scope="col">Città</th>
+                                        <th scope="col">Azioni</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($deletedApartments as $deletedApartment)
+                                        <tr class="table-danger">
+                                            <td>{{$deletedApartment->title}}</td>
+                                            <td>{{$deletedApartment->address}}</td>
+                                            <td>{{$deletedApartment->city}}</td>
+                                            <td>
+                                                <a href="{{route('admin.apartments.restore', $deletedApartment->id)}}" class="btn btn-success">Ripristina</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
                 </div>
             </div>
-        </div>
-
     </section>
 
 @endsection
