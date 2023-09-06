7fac0f6f08a069085a5d4e1bfe4be828c1bac175
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-09-06 11:39:45 +0200

    feat link

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -7,27 +7,34 @@
             <hr>
         </div>
         <div class="row mx-auto d-flex flex-wrap">
-            @foreach ($apartments as $apartment)
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="card-apartment shadow p-1 rounded-4 m-3">
-                        <a href="{{ route("admin.apartments.show", $apartment->id) }}">
-                            <div class="m-2 ">
-                                @if ($apartment->full_path_principal_image)
-                                    <img src="{{ $apartment->full_path_principal_image }}" class="rounded-4" alt="{{ $apartment->title }}">
-                                @else
-                                    <img src="https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png" class="rounded-4" alt="Placeholder Image">
-                                @endif
-                                <div class="title m-2 w-100">
-                                    {{ $apartment->title }}
-                                </div>
-                                <p class="ms-2 fw-light text-secondary">
-                                    {{ $apartment->address }}, {{$apartment->city}}
-                                </p>
-                            </div>
-                        </a>
-                    </div>
+            @if ($apartments->isEmpty())
+                <div class="col-12 text-center">
+                    <h3>Non hai ancora aggiunto nessun appartamento.</h3>
+                    <a class=" text-decoration-none text-white" href="{{ route('admin.apartments.create') }}">Aggiungi un appartamento</a>
                 </div>
-            @endforeach
+            @else
+                @foreach ($apartments as $apartment)
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="card-apartment shadow p-1 rounded-4 m-3">
+                            <a href="{{ route("admin.apartments.show", $apartment->id) }}">
+                                <div class="m-2">
+                                    @if ($apartment->full_path_principal_image)
+                                        <img src="{{ $apartment->full_path_principal_image }}" class="rounded-4" alt="{{ $apartment->title }}">
+                                    @else
+                                        <img src="https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png" class="rounded-4" alt="Placeholder Image">
+                                    @endif
+                                    <div class="title m-2 w-100">
+                                        {{ $apartment->title }}
+                                    </div>
+                                    <p class="ms-2 fw-light text-secondary">
+                                        {{ $apartment->address }}, {{ $apartment->city }}
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </section>
 @endsection
