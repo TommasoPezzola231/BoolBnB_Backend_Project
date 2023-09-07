7269c87f45cd6c9943728bc3309d3841f2429711
Valerio Billi <valerio.billi18@gmail.com>
2023-09-07 09:22:51 +0200

    style messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -7,21 +7,23 @@
 
             
             <div class="row">
-                <div class="col-4 p-0">
-                    <h3 class="mt-3">Filtra per appartamento</h3>
-                    @foreach ($apartments as $apartment)
-                    <div class="border p-4 apartment-item" data-apartment-id="{{$apartment->id}}">
-                        {{$apartment->title}}
+                <div class="col-4 d p-0 bg-light latosx">
+                    <div class="form-control p-0">
+                        <h3 class="mt-3 ms-3">Filtra per appartamento</h3>
+                        @foreach ($apartments as $apartment)
+                            <div class="p-3 apartment-item" data-apartment-id="{{$apartment->id}}">
+                            {{$apartment->title}}
+                            </div>
+                        @endforeach
                     </div>
-                    @endforeach
-                </div>
-                <div class="col-8 m-auto latosx">
                     
+                </div>
 
+                <div class="col-8 m-auto latodx">
                     <div>
 
                         @foreach ($messages as $message)
-                        <div class="message-item p-4 border" data-apartment-id="{{$message->apartment_id}}">
+                        <div class="message-item p-4" data-apartment-id="{{$message->apartment_id}}">
                             <div class="d-flex justify-content-between">
                                 <h5>{{ $message->name_sender }} {{ $message->surname_sender}}</h5>
                                  
@@ -32,7 +34,7 @@
                                 @endif
                             </div>
                             <div id="{{$message->id}}">
-                                <div class="badge bg-primary mb-3"> {{ $message->apartment_title }}</div>
+                                <a href="{{ route("admin.apartments.show", $message->apartment_id) }}" class="badge bg-primary mb-3">{{ $message->apartment_title }}</a>
                                 <p>Email: {{ $message->email_sender }}</p>
                                 <p>Oggetto: {{ $message->message_object}}</p>
                                 <p>Messaggio: {{ $message->message_text }}</p>
@@ -70,54 +72,12 @@
                         apartmentItems.forEach(function(item) {
                             item.classList.remove('selected');
                         });
+        
                         this.classList.add('selected');
                     });
                 });
             });
         </script>
-        
-        
-        
-
-
-        {{-- <h1 class="spingiGiu">Messaggi ricevuti</h1>
-
-        <form action="{{ route('admin.messages.index') }}" method="get">
-            <label for="apartment_id">Filter by Apartment:</label>
-            <select name="apartment_id" id="apartment_id">
-                <option value="">Tutti i messaggi</option>
-                @foreach ($apartments as $apartment)
-                    <option value="{{ $apartment->id }}" {{ $apartment_id == $apartment->id ? 'selected' : '' }}>
-                        {{ $apartment->title }}
-                    </option>
-                @endforeach
-            </select>
-            <button type="submit">Filter</button>
-        </form>
-
-        
-        <ul>
-            @foreach ($messages as $message)
-                <li>
-                    <h3>{{ $message->name_sender }} {{ $message->surname_sender}}</h3>
-                    
-                    @if (\Carbon\Carbon::hasFormat($message->sent_at, 'Y-m-d H:i:s'))
-                        
-                        <p>{{ \Carbon\Carbon::parse($message->sent_at)->format('m/d/Y') }}</p>
-                    @else
-                       
-                        <p>Ricevuto il: {{ $message->sent_at }}</p>
-                    @endif
-                    <p>Email cliente: {{ $message->email_sender }}</p>
-                    <p>Oggetto: {{ $message->message_object}}</p>
-                    <p>Messaggio: {{ $message->message_text }}</p>
-                </li>
-            @endforeach
-        </ul> --}}
-
-
-       
-
         
     
 @endsection
