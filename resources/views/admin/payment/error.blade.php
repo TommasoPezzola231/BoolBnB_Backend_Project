469305c4ccdbb56e3ff1e515ff3f1b1e5f294d8e
Valerio Billi <valerio.billi18@gmail.com>
2023-09-07 10:51:30 +0200

    style success and error payment

--- resources/views/admin/payment/error.blade.php
+++ resources/views/admin/payment/error.blade.php
@@ -4,14 +4,16 @@
     <div class="my-3">
         <div class="row">
             <div class="col-md-12">
-                <div class="card card-badge bg-danger">
-                    <div class="card-body">
-                        <h1>Errore nel pagamento</h1>
-                        <p>Si è verificato un errore nel pagamento. Riprova</p>
-                        <a href="{{ route('admin.sponsorships.index') }}" class="text-decoration-none text-dark">Torna alla pagina del pagamento</a>
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-circle-xmark text-danger me-3 fs-3"></i>
+                        <span class="fs-3">Errore nel pagamento!</span>
+                        <div class="fs-5 mt-2">La transazione non è andata a buon fine. La preghiamo di riprovare.</div>
+                        <a href="{{ route('admin.apartments.index') }}" class="btn btn-outline-danger mt-3">Torna alla pagina del pagamento.</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
